d9adc91092c1bca32c3765107070e6931b668f79
HP Informatique <154374204+HPInformatique@users.noreply.github.com>
2024-09-18 12:21:37 +0200

    Update masquerurlcodeverifié.js

--- masquerurlcodeverifié.js
+++ masquerurlcodeverifié.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   let stateObj = { id: "100" };
             window.history.replaceState(stateObj,
-                        "Centre d'activation - HP Informatique", "/code-vérifié");
+                        "Centre d'activation - HP Informatique", "https://hpinformatique.github.io/activation/");
 });
 
